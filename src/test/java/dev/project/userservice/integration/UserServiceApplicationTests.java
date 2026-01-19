b1137039977bce6e38cddc3eb24afd1390d381f5
rahulsur86 <rahulsur.work@gmail.com>
2026-01-19 14:10:20 +0530

    added unit tests for UserServiceApplication file

--- src/test/java/dev/project/userservice/integration/UserServiceApplicationTests.java
+++ src/test/java/dev/project/userservice/integration/UserServiceApplicationTests.java
@@ -1,5 +1,6 @@
 package dev.project.userservice.integration;
 
+import dev.project.userservice.UserServiceApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,12 @@ class UserServiceApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void mainRunsWithoutException() {
+        // call main with empty args to ensure the application class can be started
+        UserServiceApplication.main(new String[]{});
     }
 
 }
